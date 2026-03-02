3da46d4b2d309195a36b78c0708f179623bfcb12
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2026-03-01 22:50:52 -0500

    onboarding 3 and cookies has been added

---
+++ app/cookies.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const cookies = () => {
+  return (
+    <View>
+      <Text>cookies</Text>
+    </View>
+  )
+}
+
+export default cookies
+
+const styles = StyleSheet.create({})
---
+++ app/onboarding3.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const onboarding3 = () => {
+  return (
+    <View>
+      <Text>onboarding3</Text>
+    </View>
+  )
+}
+
+export default onboarding3
+
+const styles = StyleSheet.create({})
