8ddae232b00cf260ff5442978534403276961480
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2026-03-18 18:14:30 -0400

    adding input box for atlas

--- app/(tabs)/atlas.tsx
+++ app/(tabs)/atlas.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TextInput, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
@@ -6,6 +6,13 @@ const atlas = () => {
   return (
     <SafeAreaView style={styles.safeView}>
       <View style={styles.mainContainer}>
+
+        <TextInput 
+        placeholder='Ask Atlas'
+        style={styles.inputMessageContainer}
+        >
+
+        </TextInput>
         
       </View>
     </SafeAreaView>
@@ -17,11 +24,22 @@ export default atlas
 const styles = StyleSheet.create({
   safeView: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#000000',
   },
   mainContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  inputMessageContainer:{
+    width:350,
+    height:50,
+    backgroundColor:'#8f8f8f',
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    position: 'absolute',
+    bottom: 5,
+    right: 10,
   },
 })
