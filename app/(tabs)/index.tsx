cd1bb3addbaa03426ea033545715ac05fab61ed2
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2026-03-18 17:23:35 -0400

    support cards up but need to make them scroll horizontal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,49 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { router } from 'expo-router';
+import { Card, List } from 'react-native-paper';
 
 export default function index() {
   return (
     <SafeAreaView style={styles.safeView}>
       <View style={styles.mainContainer}>
 
+      <Text style={styles.supportTitle}>My Devices</Text>
+
+        <View style={styles.horizontalScrollRow}>
+          <Card style={styles.myDevicesCard}>
+              <List.Item
+                title="Techlab Account"
+                titleStyle={{ color: 'white' }}
+                onPress={() => router.push('/techlabAccount')}
+              />
+          </Card>
+
+          <Card style={styles.myDevicesCard}>
+              <List.Item
+                title="Techlab Account"
+                titleStyle={{ color: 'white' }}
+                onPress={() => router.push('/techlabAccount')}
+              />
+          </Card>
+
+          <Card style={styles.myDevicesCard}>
+              <List.Item
+                title="Techlab Account"
+                titleStyle={{ color: 'white' }}
+                onPress={() => router.push('/techlabAccount')}
+              />
+          </Card>
+
+          <Card style={styles.myDevicesCard}>
+              <List.Item
+                title="Techlab Account"
+                titleStyle={{ color: 'white' }}
+                onPress={() => router.push('/techlabAccount')}
+              />
+          </Card>
+        </View>
         
 
       </View>
@@ -22,7 +59,24 @@ const styles = StyleSheet.create({
   },
   mainContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 16,
   },
+  horizontalScrollRow:{
+    flexDirection:'row',
+  },
+  myDevicesCard:{
+    width: 200,
+    height: 100,
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: '#1C1C1E',
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: 'white',
+    marginRight:10,
+  },
+  supportTitle:{
+
+  },
+
 });
