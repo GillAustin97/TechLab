dcb4bfad860235cc259d2811471fa5a2f39f6130
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2026-03-18 16:54:12 -0400

    added atlas menu for chat bot

---
+++ app/atlasMenu.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const atlasMenu = () => {
+  return (
+    <View>
+      <Text>atlasMenu</Text>
+    </View>
+  )
+}
+
+export default atlasMenu
+
+const styles = StyleSheet.create({})
